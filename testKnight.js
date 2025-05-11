985f7188fd6bb834ba38f37d5239a73f257c118a
Maiar0 <dennis.ward2nd@gmail.com>
2025-05-10 22:10:42 -0500

    Knight Moves Logic

---
+++ testKnight.js
@@ -0,0 +1,11 @@
+const ChessBoard = require('./utils/chess/board/ChessBoard');
+
+let chessBoard = new ChessBoard("rnbqkbnr/pppppppp/8/3N4/8/8/PPPPPPPP/RNBQKBNR w KQkq c3 0 1");
+console.log(chessBoard.board); // Log the board to the console
+let piece = chessBoard.getPiece(3, 4); // Get the piece at (0, 1)
+if (piece === null) {
+    console.log("No piece found");
+    return 1;
+}
+console.log(piece.constructor.name, piece.color, piece.position); // Get the piece at (0, 0)
+console.log(piece.getMoves(chessBoard)); // Get the moves for the piece at (0, 2)
--- utils/chess/pieces/Knight.js
+++ utils/chess/pieces/Knight.js
@@ -4,5 +4,34 @@ class Knight extends ChessPiece {
     constructor(color, position) {
         super(color, position); // Call the constructor of the parent class (ChessPiece)
     }
+    getMoves(board) {
+        let moves = []; // Array to store possible moves
+        const directions = [
+            { dx: 2, dy: 1 }, // Up Right
+            { dx: 2, dy: -1 }, // Down Right
+            { dx: -2, dy: 1 }, // Up Left
+            { dx: -2, dy: -1 }, // Down Left
+            { dx: 1, dy: 2 }, // Up Right
+            { dx: 1, dy: -2 }, // Down Right
+            { dx: -1, dy: 2 }, // Up Left
+            { dx: -1, dy: -2 } // Down Left
+        ];
+        for (const {dx, dy} of directions) {
+            let x = this.position.x + dx;
+            let y = this.position.y + dy;
+            if (board.boundsCheck(x, y)) {
+                const piece = board.getPiece(x,y); // Get the piece at the new position
+                if(piece === null ){
+                    moves.push({ x: x, y: y, capture: false }); // Add the move to the list if the square is empty
+                }else{
+                    if(piece.color !== this.color){
+                        moves.push({ x: x, y: y, capture: true }); // Capture
+                    }
+                }
+            }
+        }
+        return moves; // Return the array of possible moves
+    }
+    
 }
 module.exports = Knight; // Export the Rook class
