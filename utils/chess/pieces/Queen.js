83b81a1cee2dbcfeccc7742d60131051aa2d1f96
Maiar0 <dennis.ward2nd@gmail.com>
2025-05-10 21:55:20 -0500

    Queen Move Logic

--- utils/chess/pieces/Queen.js
+++ utils/chess/pieces/Queen.js
@@ -4,5 +4,37 @@ class Queen extends ChessPiece {
     constructor(color, position) {
         super(color, position); // Call the constructor of the parent class (ChessPiece)
     }
+    getMoves(board) {
+        let moves = []; // Array to store possible moves
+        const directions = [
+            { dx: 0, dy: 1 }, // Up
+            { dx: 0, dy: -1 }, // Down
+            { dx: 1, dy: 0 }, // Right
+            { dx: -1, dy: 0 }, // Left
+            { dx: 1, dy: 1 }, // Up Right
+            { dx: -1, dy: 1 }, // Up left
+            { dx: -1, dy: -1 }, // Down left
+            { dx: 1, dy: -1 } // Down Right
+        ];
+        for  (const {dx, dy} of directions) {
+            let x = this.position.x + dx;
+            let y = this.position.y + dy;
+            console.log("x: ", x, "y: ", y);
+            while (board.boundsCheck(x, y)) {
+                const piece = board.getPiece(x,y); // Get the piece at the new position
+                if(piece === null ){
+                    moves.push({ x: x, y: y, capture: false }); // Add the move to the list if the square is empty
+                }else{
+                    if(piece.color !== this.color){
+                        moves.push({ x: x, y: y, capture: true }); // Capture
+                    }
+                    break; //Stop moving in this direction if a piece is encountered
+                }
+                x += dx; // Move in the x direction
+                y += dy; // Move in the y direction
+            }
+        }
+        return moves; // Return the array of possible moves
+    }
 }
 module.exports = Queen; // Export the Rook class
